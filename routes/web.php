82226e38a19dfc96d17c26c69df4e942aa7e12b9
skillbasedit <moreinfo.sbit@gmail.com>
2022-10-26 02:22:46 +0600

    newchange

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/aaa','userRegistration@index');
 Route::get('/logins','user\homeController@userlogin');
 Route::get('/practice','user\homeController@practice');
-Route::get('/practice2','user\homeController@practice');
 
 Route::resources([
 	'reg'=>'userRegistration',
