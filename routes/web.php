93773903d0185474a3235c8554d1aededb730c3f
sourobuiu <sourobuiu@gmail.com>
2022-10-26 01:25:33 +0600

    changes..

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/abc','userRegistration@index');
-Route::get('/login','user\homeController@userlogin');
+Route::get('/login','user\homeController@userlogins');
 Route::get('/practice','user\homeController@practice');
 Route::get('/practice2','user\homeController@practice');
 
